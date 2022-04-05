ec6a212aa0080c50a14535f0b60fc74659895ba3
rerang <chemic00@naver.com>
2022-04-05 11:58:53 +0900

    edited test

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   content: ["./public/src/**/**.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': ['Gowun Dodum', 'ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'basic':['Gowun Dodum'],
+    },
   },
   plugins: [],
 }
