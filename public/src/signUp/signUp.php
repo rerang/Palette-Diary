e17fca7138d0d0c7ddb92a5a178ab7281b360380
chfhd5501 <35456249+chfhd5501@users.noreply.github.com>
2022-05-10 12:40:50 +0900

    Update signUp.php

--- public/src/signUp/signUp.php
+++ public/src/signUp/signUp.php
@@ -6,28 +6,31 @@ header("Access-Control-Allow-Origin: *");
 $host = "localhost";
 $s_username = "db";
 $s_password = "dbpassword";
-$dbname = "palette_diary";
+$dbname = "test";
 $conn = mysqli_connect($host, $s_username, $s_password, $dbname);
 
 //var for func
 $user_type = "user";
 
-try{
+try{/*
   $json = json_decode(file_get_contents('php://input'), TRUE);
 
   $email = $json['email'];
-  $password = $json['password'];
+  $password = $json['password'];*/
+  $email="sdfjwe@naver.com";
+  $password="sdfsdff123!";
   $error = "none";
   $stat = "none";
 
   $checkingEmailExistSql="select * from user where email='$email'";
-  $checkingEmailExistResult = mysqli_query($conn, $checkingEmailExistSql);
-  if($checkingEmailExistResult->lengths) { //null이아니라면
+  $checkingEmailExistResult = mysqli_fetch_assoc(mysqli_query($conn, $checkingEmailExistSql));
+  if(empty($checkingEmailExistResult) == false){
     throw new exception('email has exist', 401);
-    exit();
   }
 
-  $insertUserSql = "insert into user(email, password, user_type) values('$email', '$password', '$user_type');";
+  $encrypted_password = password_hash($password, PASSWORD_DEFAULT); //password 암호화
+
+  $insertUserSql = "insert into user(email, password, user_type, theme_code) values('$email', '$encrypted_password', '$user_type','1');";
   $insertResult = mysqli_query($conn, $insertUserSql);
 
   if($insertResult){
@@ -37,8 +40,8 @@ try{
     throw new exception('cant insert user', 400);
   }
 }catch(exception $e) {
-  $stat	= "error";
-  $error = ['errorMsg'	=> $e->getMessage(), 'errorCode' => $e->getCode()];
+  $stat   = "error";
+  $error = ['errorMsg'   => $e->getMessage(), 'errorCode' => $e->getCode()];
 }finally{
   $data =  json_encode(['result_code' => $stat, 'error'=>$error]);
   header('Content-type: application/json'); 
