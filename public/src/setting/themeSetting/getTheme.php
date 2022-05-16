113556c828e67e51f84a698582dd6557abf15b11
chfhd5501 <35456249+chfhd5501@users.noreply.github.com>
2022-05-16 23:47:46 +0900

    Update getTheme.php

--- public/src/setting/themeSetting/getTheme.php
+++ public/src/setting/themeSetting/getTheme.php
@@ -17,24 +17,29 @@ try{
     $selectThemeSql = "select * from theme;";
     $selectThemeResult=mysqli_query($conn, $selectThemeSql);
     if(empty($selectThemeResult)==true) { //null이라면
-        throw new exception('theme 테이블 정보를 불러올 수 없음', 402);
+        throw new exception('theme 테이블 정보를 불러올 수 없음', 404);
     }
-    
+    else{
     $codeArr=array();
     $backgroundArr=array();
 
-    while ($test = mysqli_fetch_assoc($selectThemeResult)){
-        array_push($codeArr, $test['theme_code']);
-        array_push($backgroundArr, $test['background_pic']);
+    while ($themeRecord = mysqli_fetch_assoc($selectThemeResult)){
+        array_push($codeArr, $themeRecord['theme_code']);
+        array_push($backgroundArr, $themeRecord['background_pic']);
     }
 
+    $test1=json_encode($codeArr);
+    $test2=json_encode($backgroundArr);
+
     mysqli_close($conn);
+    $stat="success";
+}
 
 }catch(exception $e) {
     $stat   = "error";
     $error = ['errorMsg'   => $e->getMessage(), 'errorCode' => $e->getCode()];
 }finally{
-    $data =  json_encode(['theme_code' => $codeArr, 'background_pic' => $backgroundArr, 'result_code' => $stat, 'error'=>$error]);
+    $data =  json_encode(['theme_code' => $test1, 'background_pic' => $test2, 'result_code' => $stat, 'error'=>$error]);
     header('Content-type: application/json'); 
     echo $data;
 }
