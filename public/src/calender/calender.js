05f0e412d7b1fc4a1622bf2c010cabb141c47727
rerang <chemic00@naver.com>
2022-05-25 14:35:59 +0900

    happyDiary close btn

---
+++ public/src/calender/calender.js
@@ -0,0 +1,20 @@
+//onload
+const cookieTarget = "token";
+let token = "";
+document.cookie.split(";").forEach(ele => {
+    if(ele.split("=")[0].trim() == cookieTarget){
+        token = ele.split("=")[1];
+    }
+})
+
+if(token==""){
+  window.location.href = "http://125.140.42.36:8082";
+}
+const payload = JSON.parse(atob(token.split('.')[1]));
+const user_type = payload['user_type'];
+
+if(user_type == "admin"){
+  window.location.href = "http://125.140.42.36:8082/public/src/admin/admin.html";
+}
+
+//calender get now month info
---
+++ public/src/happyBank/readHappyDiary.js
@@ -0,0 +1,28 @@
+//onload
+const cookieTarget = "token";
+let token = "";
+document.cookie.split(";").forEach(ele => {
+    if(ele.split("=")[0].trim() == cookieTarget){
+        token = ele.split("=")[1];
+    }
+})
+
+if(token==""){
+  window.location.href = "http://125.140.42.36:8082";
+}
+const payload = JSON.parse(atob(token.split('.')[1]));
+const user_type = payload['user_type'];
+
+if(user_type == "admin"){
+  window.location.href = "http://125.140.42.36:8082/public/src/admin/admin.html";
+}
+
+//get diary
+
+
+//close happy diary
+const happyBankDiaryCloseBtn = document.querySelector("#happyBankDiaryCloseBtn");
+const goHappyDiaryPage = () => {
+    window.location.href = "http://125.140.42.36:8082/public/src/happyBank/happyBank.html";
+}
+happyBankDiaryCloseBtn.addEventListener("click", goHappyDiaryPage);
