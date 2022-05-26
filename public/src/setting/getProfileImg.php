89c4dacd810c4c4512ce73d0bb274d9fde0633ca
chfhd5501 <35456249+chfhd5501@users.noreply.github.com>
2022-05-27 01:26:15 +0900

    Update getProfileImg.php

--- public/src/setting/getProfileImg.php
+++ public/src/setting/getProfileImg.php
@@ -33,18 +33,6 @@ try{
     $imgPath = $getUserInfoResult['profile_pic'];
   
     if($getUserInfoResult) { 
-
-        $conn_id = ftp_connect($ftp_server, $ftp_port);
-        ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
-        ftp_pasv($conn_id, true);
-
-        $remote_file = $imgPath;
-        $local_file = "./userProfile/".$fileName;
-        $fp = fopen($local_file, 'w+');
-        ftp_fget($conn_id, $fp, $remote_file, FTP_BINARY, 0);
-        ftp_close($conn_id);
-        fclose($fp);
-
         $stat = "success";
     }
     else {
@@ -54,7 +42,7 @@ try{
     $stat = "error";
     $error = ['errorMsg' => $e->getMessage(), 'errorCode' => $e->getCode()];
 }finally{
-    $data = json_encode(['imgPath' => $local_file, 'result_code' => $stat, 'error'=> $error]);
+    $data = json_encode(['imgPath' => $imgPath, 'result_code' => $stat, 'error'=> $error]);
     header('Content-type: application/json'); 
     echo $data;
 }
