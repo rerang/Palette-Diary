2495cb5663b4245195468b11e4f287e9532fa0d4
chfhd5501 <35456249+chfhd5501@users.noreply.github.com>
2022-05-27 00:48:22 +0900

    Update getProfileImg.php

--- public/src/setting/getProfileImg.php
+++ public/src/setting/getProfileImg.php
@@ -9,6 +9,12 @@ $s_password = "dbpassword";
 $dbname = "palette_diary";
 $conn = mysqli_connect($host, $s_username, $s_password, $dbname);
 
+//FTP connect
+$ftp_server = "125.140.42.36";
+$ftp_port = 21;
+$ftp_user_name = "paletteDiary";
+$ftp_user_pass = "paletteDiary";
+
 try{
     $json = json_decode(file_get_contents('php://input'), TRUE);
     $error = "none";
@@ -26,17 +32,29 @@ try{
     
     $imgPath = $getUserInfoResult['profile_pic'];
   
-    if($getUserInfoResult){ 
+    if($getUserInfoResult) { 
+
+        $conn_id = ftp_connect($ftp_server, $ftp_port);
+        ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
+        ftp_pasv($conn_id, true);
+
+        $remote_file = $imgPath;
+        $local_file = "./userProfile/".$fileName;
+        $fp = fopen($local_file, 'w+');
+        ftp_fget($conn_id, $fp, $remote_file, FTP_BINARY, 0);
+        ftp_close($conn_id);
+        fclose($fp);
+
         $stat = "success";
     }
-    else{
+    else {
         throw new exception('DB Fail - Can Not Get Profile Pic', 422);
     }
 }catch(exception $e) {
     $stat = "error";
     $error = ['errorMsg' => $e->getMessage(), 'errorCode' => $e->getCode()];
 }finally{
-    $data = json_encode(['imgPath' => $imgPath, 'result_code' => $stat, 'error'=> $error]);
+    $data = json_encode(['imgPath' => $local_file, 'result_code' => $stat, 'error'=> $error]);
     header('Content-type: application/json'); 
     echo $data;
 }
