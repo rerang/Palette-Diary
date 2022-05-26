5d9bb3d468ea9b173dba14aa51520535a24c4dcc
kmonasu <102565390+kmonasu@users.noreply.github.com>
2022-05-26 14:59:58 +0900

    Create calendar.php
    월간조회 일,색깔 배열순으로 조회

---
+++ public/src/calender/calendar.php
@@ -0,0 +1,59 @@
+<?php
+//to solve cors issue
+header("Access-Control-Allow-Origin: *");
+
+ //db connect
+$host = "localhost";
+$s_username = "db";
+$s_password = "dbpassword";
+$dbname = "palette_diary";
+$conn = mysqli_connect($host, $s_username, $s_password, $dbname);
+
+$user_type = "user";
+
+try{
+    $json = json_decode(file_get_contents('php://input'), TRUE);
+    $error = "none";
+    $stat = "none";
+
+    $cookie = apache_request_headers()['Cookie'];
+    $email = json_decode(base64_decode(str_replace('_', '/', str_replace('-', '+', explode('.', explode("=", $cookie)[1])[1]))), TRUE)['email'];
+
+    $checkingEmailExistSql="select * from user where email='$email'";
+    $checkingEmailExistResult = mysqli_fetch_assoc(mysqli_query($conn, $checkingEmailExistSql));
+    
+    $d_date = $json['d_date'];
+
+    $checkSql="select d_date,color from diary where USER_email='$email'and substr(d_date,3,5)='$d_date' order by d_date DESC;";  
+    // d_date의 ****-**-** 중 3번째 년도~ 월까지 5개 , 내림차순으로 1일->2일 이런식으로 정렬
+    $checkResult = mysqli_query($conn, $checkSql);
+    $checkRow = mysqli_fetch_assoc($checkResult);
+
+    if(empty($checkRow)==true) { 
+        throw new exception('checking no d_date', 422);
+    }
+    else {
+        $dataArr = array();
+        $colorArr = array();
+
+        while($checkRow = mysqli_fetch_assoc($checkResult)){
+            $StrCheckRow=(string)$checkRow['d_date'];
+            $DBd_date = substr($StrCheckRow,3,5);
+            array_push($dataArr,$DBd_date);
+            array_push($colorArr,$checkRow['color']);
+        }
+        mysqli_close($conn);
+        $stat="success";
+    }
+    
+}catch(exception $e) {
+    $stat = "error";
+    $error = ['errorMsg' => $e->getMessage(), 'errorCode' => $e->getCode()];
+}finally{
+    $data = json_encode(['d_date' => $dataArr, 'color' => $colorArr,'result_code' => $stat, 'error'=> $error ]);
+    header('Content-type: application/json'); 
+    echo $data;
+}
+
+mysqli_close($conn);
+?>
