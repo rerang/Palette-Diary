ca626d7d9832fb92c05c139a7ba5aeebeb9ec5e8
chfhd5501 <35456249+chfhd5501@users.noreply.github.com>
2022-09-25 23:12:01 +0900

    Update wordCloud.py

--- public/src/colorStat/wordCloud.py
+++ public/src/colorStat/wordCloud.py
@@ -15,7 +15,7 @@ print(dict)
 file.close()
 
 #MalangmalangR 폰트 없으면 다른 폰트로 교체, 팔레트 색상도 고려중
-wc = WordCloud(font_path='C:\Windows\Fonts\MalangmalangR.ttf', background_color='white', colormap="Pastel1", width=400, height=400, scale=2.0, max_font_size=150)
+wc = WordCloud(font_path='C:\Windows\Fonts\malgun.ttf', background_color='white', colormap="Pastel1", width=400, height=400, scale=2.0, max_font_size=150)
 gen = wc.generate_from_frequencies(dict)
 
 
