f73ea938e627ae5e4f7a373ffa45fe20581d0a79
chfhd5501 <35456249+chfhd5501@users.noreply.github.com>
2022-11-01 10:24:20 +0900

    Update wordCloud.py

--- public/src/colorStat/wordCloud.py
+++ public/src/colorStat/wordCloud.py
@@ -1,8 +1,11 @@
-from array import array
+#from array import array
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
-import os
-import requests
+import numpy as np
+from PIL import Image
+from PIL import *
+import warnings
+warnings.filterwarnings('ignore')
 
 file = open('./KeywordData.txt','r', encoding='UTF-8')
 x = file.read()
@@ -14,13 +17,16 @@ dict=dict(zip(c,cc))
 print(dict)
 file.close()
 
-#MalangmalangR 폰트 없으면 다른 폰트로 교체, 팔레트 색상도 고려중
-wc = WordCloud(font_path='C:\Windows\Fonts\malgun.ttf', background_color='white', colormap="Pastel1", width=400, height=400, scale=2.0, max_font_size=150)
+mask2=np.array(Image.open('tte2.jpg'))
+
+#wc = WordCloud(font_path='./BMHANNA_11yrs_ttf.ttf', background_color='white', mask=mask2, colormap="Pastel1", width=400, height=400, scale=2.0, max_font_size=150)
+wc = WordCloud(font_path='./BMHANNA_11yrs_ttf.ttf', background_color='white', colormap="Pastel1", width=400, height=400, scale=2.0, max_font_size=100)
 gen = wc.generate_from_frequencies(dict)
 
 
 plt.figure(figsize=(20,20))
 plt.imshow(gen)
 plt.axis('off')
-plt.show()
-wc.to_file('KeywordWordcloud.png')
+#plt.show()
+plt.savefig('KeywordWordcloud.png')
+#wc.to_file('KeywordWordcloud.png')
