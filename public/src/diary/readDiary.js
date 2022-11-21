e08a9778c171c01876818a0221d8aa1075e7170f
rerang <chemic00@naver.com>
2022-11-22 02:18:00 +0900

    read diary

--- public/src/diary/readDiary.js
+++ public/src/diary/readDiary.js
@@ -21,6 +21,45 @@ const expired = () => {
     window.location.href = "http://125.140.42.36:8082/";
 }
 
+//get and paint diary
+const diaryYear = document.querySelector("#diaryYear");
+const diaryMonth = document.querySelector("#diaryMonth");
+const diaryDay = document.querySelector("#diaryDay");
+const diaryColor = document.querySelector("#diaryColor");
+const diaryKeyword = document.querySelector("#diaryKeyword");
+const diaryMainPic = document.querySelector("#diaryMainPic");
+const diarySubPic1 = document.querySelector("#diarySubPic1");
+const diarySubPic2 = document.querySelector("#diarySubPic2");
+const diaryBodyArea = document.querySelector("#diaryBodyArea");
+
+
+const paintDiary = (diaryData) => {
+  const dateArr = diaryData.date.split("-");
+  diaryYear.innerHTML = dateArr[0];
+  diaryMonth.innerHTML = dateArr[1];
+  diaryDay.innerHTML = dateArr[2];
+
+  const colorAttr = "background-color:"+diaryData.color;
+  diaryColor.setAttribute("style", colorAttr);
+
+  diaryKeyword.innerHTML = diaryData.keyword;
+  if(diaryData.mainPic !== null){
+    let imgurl = diaryData.mainPic == "" ? "../../assets/noPic.png" : diaryData.mainPic;
+    diaryMainPic.setAttribute("src", imgurl);
+  }
+  if(diaryData.subPic1 !== null){
+    let imgurl = diaryData.subPic1 == "" ? "../../assets/noPic.png" : diaryData.subPic1;
+    diarySubPic1.setAttribute("src", imgurl);
+  }
+  if(diaryData.subPic2 !== null){
+    let imgurl = diaryData.subPic2 == "" ? "../../assets/noPic.png" : diaryData.subPic2;
+    diarySubPic2.setAttribute("src", imgurl);
+  }
+
+  diaryBodyArea.innerHTML = diaryData.diary_body;
+}
+
+
 //get diary
 const viewDiaryUrl = "http://125.140.42.36:8082/public/src/diary/getDiary.php";
 const readDiaryCode = localStorage.getItem("readDiaryCode");
@@ -37,10 +76,10 @@ const getDiary = async() => {
     })
     const data = res.json();
     data.then(
-        dataResult => {
-            if(dataResult.result_code == "success"){  
-              paintDiary({date:dataResult.d_date, color:dataResult.color, keyword:dataResult.keyword, mainPic:dataResult.mainPic, diary_body:dataResult.diary_body, subPic1:dataResult.subPic1, subPic2:dataResult.subPic2});
-            }
+        dataResult => {paintDiary({date:dataResult.d_date, color:dataResult.color, keyword:dataResult.keyword, mainPic:dataResult.mainPic, diary_body:dataResult.diary_body, subPic1:dataResult.subPic1, subPic2:dataResult.subPic2});
+            
+            if(dataResult.result_code == "success"){ 
+              }
             else if(dataResult.error.errorCode == 423){
               expired();
             }
@@ -51,45 +90,10 @@ const getDiary = async() => {
   } 
 }
 
-const diaryYear = document.querySelector("#diaryYear");
-const diaryMonth = document.querySelector("#diaryMonth");
-const diaryDay = document.querySelector("#diaryDay");
-const diaryColor = document.querySelector("#diaryColor");
-const diaryKeyword = document.querySelector("#diaryKeyword");
-const diaryMainPic = document.querySelector("#diaryMainPic");
-const diarySubPic1 = document.querySelector("#diarySubPic1");
-const diarySubPic2 = document.querySelector("#diarySubPic2");
-const diaryBodyArea = document.querySelector("#diaryBodyArea");
-
-const paintDiary = (diaryData) => {
-  const onlyDate = diaryData['date'].split(" ")[0];
-  const onlyDateArr = onlyDate.split("-");
-  diaryYear.innerHTML = onlyDateArr[0];
-  diaryMonth.innerHTML = onlyDateArr[1];
-  diaryDay.innerHTML = onlyDateArr[2];
-
-  const colorAttr = "background-color:"+diaryData['color']+";";
-  diaryColor.setAttribute("style", colorAttr);
-
-  diaryKeyword.innerHTML = diaryData['keyword'];
-  if(diaryData['mainPic'] !== null){
-    diaryMainPic.setAttribute("src", diaryData['mainPic']);
-  }
-  if(diaryData['subPic1'] !== null){
-    console.log(diaryData['subPic1']);
-    diarySubPic1.setAttribute("src", diaryData['subPic1']);
-  }
-  if(diaryData['subPic2'] !== null){
-    diarySubPic2.setAttribute("src", diaryData['subPic2']);
-  }
-
-  diaryBodyArea.innerHTML = diaryData['diary_body'];
-}
-
 getDiary();
 
-
-const modalBg = document.querySelector("#modalBg");
+//delete
+const diaryModalBg = document.querySelector("#diaryModalBg");
 const deleteDiaryUrl = `http://125.140.42.36:8082/public/src/diary/deleteDiary.php`;
 const diaryDeleteBtn = document.querySelector("#diaryDeleteBtn");
 
@@ -101,7 +105,7 @@ const askDeleteDiary = (_event) => {
   <button class="askDeleteDiaryBtn" id="askDeleteDiaryTrueBtn">예</button>
   <button class="askDeleteDiaryBtn" id="askDeleteDiaryFalseBtn">아니오</button>
   </div>`;
-  calenderModalBg.classList.remove("hidden");
+  diaryModalBg.classList.remove("hidden");
   document.querySelector("body").appendChild(askDeleteDiaryModal);
   document.querySelector("#askDeleteDiaryTrueBtn").addEventListener("click", deleteDiary, true);
   document.querySelector("#askDeleteDiaryFalseBtn").addEventListener("click", removeModal, true);
@@ -140,8 +144,7 @@ const deleteDiary = async() => {
   }
 }
 const removeModal = () => {
-  askDeleteDiaryModal.remove();
-  calenderModalBg.classList.add("hidden");
+  diaryModalBg.classList.add("hidden");
 }
 const goCalenderPage = (_event) => {
     localStorage.removeItem("readDiaryCode", readDiaryCode);
